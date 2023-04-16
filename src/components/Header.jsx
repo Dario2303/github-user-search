9b77a70f2fd8c02c33628ca2491df12c76bb7705
dariorod <dario2303@gmail.com>
2023-04-16 15:42:29 -0300

    work in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import imagen from '../img/icon-moon.svg'
 
 const Head = styled.div `
-    display: flex;
+    display: flex;   
+`
+const DarkLight = styled.p `
+  text-transform: uppercase;
+`
 
+const ThemeIcon = styled.div `
+  &::before{  
+    content: '';
+    background-image: url(${imagen});
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 3rem;
+    height: 3rem;
+    display: block;
+    }
+`
+
+const ThemeMenu = styled.div `
+  display: flex;
+  margin-left: auto;
+  align-items: center;
 `
 
 const Header = () => {
   return (
     <Head>
       <h1>devfinder</h1>
-      <div>
-        <p>dark</p>
-      </div>
+      <ThemeMenu>
+        <DarkLight>dark</DarkLight>
+        <ThemeIcon></ThemeIcon>
+      </ThemeMenu>
     </Head>
   )
 }
